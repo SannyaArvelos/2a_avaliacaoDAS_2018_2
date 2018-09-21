2bf573ba6e5805fc0aa7c11c31a014ab61cb58bb
SannyaArvelos <sannyasantana@gmail.com>
2018-09-21 17:12:06 -0300

    Segundo commit.

--- src/avaliacaoDAS_P2/Calculadora.java
+++ src/avaliacaoDAS_P2/Calculadora.java
@@ -2,7 +2,11 @@ package avaliacaoDAS_P2;
 
 public class Calculadora {
 	public static Aplicacao rendimento(float valorEntrada) {
-		return new Aplicacao(20, 34);
+		
+		if (valorEntrada > 0) {
+			return new Aplicacao(60, 22.5f);
+		}else 
+			return new Aplicacao(0, 0);
+		
 	}
 }
-
