84875e305dd2838a72edd16ac25a13c99f9322fa
SannyaArvelos <sannyasantana@gmail.com>
2018-09-21 17:24:37 -0300

    Add files via upload

--- src/avaliacaoDAS_P2/Calculadora.java
+++ src/avaliacaoDAS_P2/Calculadora.java
@@ -3,10 +3,15 @@ package avaliacaoDAS_P2;
 public class Calculadora {
 	public static Aplicacao rendimento(float valorEntrada) {
 		
-		if (valorEntrada > 0) {
-			return new Aplicacao(60, 22.5f);
-		}else 
-			return new Aplicacao(0, 0);
+		if (valorEntrada > 1) {
+			return new Aplicacao(180, 22.5f);
+		}else if (valorEntrada > 1) {
+			return new Aplicacao(360, 20f);
+		}else if (valorEntrada > 1) {
+			return new Aplicacao(720, 17.5f);
+		}else {
+			return new Aplicacao(721, 15f);
+		}
 		
 	}
 }
